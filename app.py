df91d74f8a91e2ad50907a8598afe7ee4cbf8359
Kelly Lockhart <2926089+kelockhart@users.noreply.github.com>
2022-09-26 12:05:35 -0400

    Path and app fixes

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, request, url_for, render_template
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/audio-player/static' )
 
 def create_app(**config):
   return app
 
-@app.route("/player", methods=["POST"])
+@app.route("/audio-player", methods=["POST"])
 def player():
-  audios = json.loads(request.form["sources"])
-  audio_list = [] # in the format [[title, source], ...]
-  for title, source in audios.items():
-    audio_list.append([title, source])
-  style = url_for("static", filename="styles.css")
-  script = url_for("static", filename="player.js")
-  return render_template("player.html", audios=audio_list, style=style, script=script)
+    audios = json.loads(request.form["sources"])
+    audio_list = [] # in the format [[title, source], ...]
+    for title, source in audios.items():
+        audio_list.append([title, source])
+    style = url_for("static", filename="styles.css")
+    script = url_for("static", filename="player.js")
+    return render_template("player.html", audios=audio_list, style=style, script=script)
